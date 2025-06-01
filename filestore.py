7cefade327894461245e8f9da3cc7445ad7438d5
eshaan <eshaan2031@icloud.com>
2025-05-31 23:06:37 -0500

    Create filestore.py

---
+++ filestore.py
@@ -0,0 +1,21 @@
+import os, json
+
+filename = ".fs"
+
+def set(name, value):
+  if os.path.exists(filename):
+    with open(filename, "r") as file:
+      data = json.load(file)
+  else:
+    data = {}
+  data.update({name: value})
+  with open(filename, "w") as file:
+    json.dump(data, file, indent=4)
+def get(name):
+  if os.path.exists(filename):
+    with open(filename, "r") as file:
+      data = json.load(file)
+      return data[name]
+  else:
+    return None
+      
